7606fd3abfd541dd0e92b90f54fd4ac99daba360
Falikor <Bugorv90@mail.ru>
2021-05-19 21:13:32 +0300

    Did dayFinder

--- dayFinder/ViewController.swift
+++ dayFinder/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dataTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    
+    @IBOutlet weak var resultLabl: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        guard let day = dataTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        let calendar = Calendar.current
+        var dateComponets = DateComponents()
+        dateComponets.day = Int(day)
+        dateComponets.month = Int(month)
+        dateComponets.year = Int(year)
+        
+        let dateFormator = DateFormatter()
+        dateFormator.locale = Locale(identifier: "ru_Ru")
+        dateFormator.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponets) else { return }
+        
+        let weekday = dateFormator.string(from: date)
+        let capitalizedWekday = weekday.capitalized
+        
+        resultLabl.text = capitalizedWekday
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
